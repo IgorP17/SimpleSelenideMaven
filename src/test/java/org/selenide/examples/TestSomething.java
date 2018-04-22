b72c497778f8cea0fb31e74450823485a11a03cc
Игорь Прохоров <iprokhorov@mobicont.ru>
2018-04-22 10:32:17 +0300

    -Configuration import

--- src/test/java/org/selenide/examples/TestSomething.java
+++ src/test/java/org/selenide/examples/TestSomething.java
@@ -1,6 +1,5 @@
 package org.selenide.examples;
 
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.ElementsCollection;
 import org.junit.Test;
 import org.junit.runner.RunWith;
